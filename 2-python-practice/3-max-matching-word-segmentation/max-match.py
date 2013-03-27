b28d0bc7fa352672c284742ee491756dc03109eb
Oneplus <oneplus.lau@gmail.com>
2013-03-27 23:27:00 +0800

    add max matching word segmentation

---
+++ 2-python-practice/3-max-matching-word-segmentation/max-match.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import cPickle as pickle
+import sys
+
+def max_match_segment( line, dic ):
+    # write your code here
+
+if __name__=="__main__":
+
+    try:
+        fpi=open(sys.argv[1], "r")
+    except:
+        print >> sys.stderr, "failed to open file"
+        sys.exit(1)
+
+    try:
+        dic = pickle.load(sys.argv[2])
+    except:
+        print >> sys.stderr, "failed to load dict"
+        sys.exit(1)
+
+    for line in fpi:
+        print "\t".join( max_match_segment(line.strip(), dic) )
+
